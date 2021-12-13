7b9820312fac480b4d31d8e58a1988389093f15a
pratikw008 <pratikwankhede008@gmail.con>
2021-12-13 09:27:25 +0530

    First Commit

---
+++ src/main/java/com/mb/app/ws/SpringbootMbappWsApplication.java
@@ -0,0 +1,13 @@
+package com.mb.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMbappWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMbappWsApplication.class, args);
+	}
+
+}
